883f5e04c54ca7b197ce189937f1c9c93f518e50
Wahyu Candra Tama <wahyucandratama@gmail.com>
2024-08-06 11:46:09 +0700

    Add feature - Project Section

--- src/layouts/footers/LayoutDefault.js
+++ src/layouts/footers/LayoutDefault.js
@@ -13,97 +13,37 @@ const DefaultFooter = ( { extraClass } ) => {
         <div className="mi-invert-fix">
             <div className="container pt-5">
                 <div className="row justify-content-between">
-                    <div className="col-md-4 col-lg-4 mil-mb-10">
+                    <div className="col-md-6 col-lg-6 mil-mb-60 mt-4">
 
                         <div className="mil-muted mil-logo mil-up mil-mb-10">{AppData.footer.logo.text}</div>
-
-                        {/*<p className="mil-light-soft mil-up mil-mb-30">Subscribe our newsletter:</p>*/}
-
-                        {/*<form action={AppData.settings.mailchimp.url} method="post" target="_blank" className="mil-subscribe-form mil-up">*/}
-                        {/*    <input type="email" placeholder="Enter our email" name="EMAIL" required />*/}
-                        {/*    <input type="hidden" name={AppData.settings.mailchimp.key} />*/}
-                        {/*    <button type="submit" className="mil-button mil-icon-button-sm mil-arrow-place">*/}
-                        {/*        <ArrowIcon />*/}
-                        {/*    </button>*/}
-                        {/*</form>*/}
-
-                    </div>
-                    <div className="col-md-7 col-lg-6">
-                        <div className="row justify-content-end">
-                            {/*<div className="col-md-6 col-lg-7">*/}
-
-                                {/*    <nav className="mil-footer-menu mil-mb-60">*/}
-                                {/*        <ul>*/}
-                                {/*            {AppData.footer.menu.map((item, key) => (*/}
-                                {/*            <li key={`footer-menu-item-${key}`} className={((asPath.indexOf( item.link ) != -1 && item.link != '/' ) || asPath == item.link ) ? "mil-active mil-up" : "mil-up"}>*/}
-                                {/*                <Link href={item.link}>{item.label}</Link>*/}
-                                {/*            </li>*/}
-                                {/*            ))}*/}
-                                {/*        </ul>*/}
-                                {/*    </nav>*/}
-
-                                {/*</div>*/}
-                                {/*<div className="col-md-6 col-lg-5">*/}
-
-                                {/*    <ul className="mil-menu-list mil-up mil-mb-60">*/}
-                                {/*        <li><a href="#." className="mil-light-soft">Privacy Policy</a></li>*/}
-                                {/*        <li><a href="#." className="mil-light-soft">Terms and conditions</a></li>*/}
-                                {/*        <li><a href="#." className="mil-light-soft">Cookie Policy</a></li>*/}
-                                {/*        <li><a href="#." className="mil-light-soft">Careers</a></li>*/}
-                                {/*    </ul>*/}
-
-
-
-                            {/*</div>*/}
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row justify-content-between flex-sm-row-reverse">
-                    <div className="col-md-7 col-lg-6">
-
                         <div className="row flex-row justify-content-between">
-                            <p className="mil-light-soft mil-up d-flex justify-content-end">Leehove 40, 2678 MC De Lier,
+                            <p className="mil-light-soft mil-up">Leehove 40, 2678 MC De Lier,
                                 Netherlands <span
                                     className="mil-no-wrap">+31 174 705 811</span></p>
 
-                            {/*<div className="col-md-6 col-lg-5 mil-mb-60">*/}
-
-                            {/*    <h6 className="mil-muted mil-up mil-mb-30">Canada</h6>*/}
-
-                            {/*    <p className="mil-light-soft mil-up">71 South Los Carneros Road, California <span className="mil-no-wrap">+51 174 705 812</span></p>*/}
-
-                            {/*</div>*/}
-                            {/*<div className="col-md-6 col-lg-5 mil-mb-60">*/}
-
-                            {/*    <h6 className="mil-muted mil-up mil-mb-30">Germany</h6>*/}
-
-                            {/*    <p className="mil-light-soft mil-up">Leehove 40, 2678 MC De Lier, Netherlands <span className="mil-no-wrap">+31 174 705 811</span></p>*/}
-
-                            {/*</div>*/}
                         </div>
 
-                    </div>
-                    <div className="col-md-4 col-lg-6 mil-mb-60">
-
                         <div className="mil-vert-between">
-                        {/*<div className="mil-mb-30">*/}
-                            {/*    <ul className="mil-social-icons mil-up">*/}
-                            {/*        {AppData.social.map((item, key) => (*/}
-                            {/*        <li key={`footer-social-item-${key}`}><a href={item.link} target="_blank" className="social-icon"><i className={item.icon} /></a></li>*/}
-                            {/*        ))}*/}
-                            {/*    </ul>*/}
-                            {/*</div>*/}
                             <p className="mil-light-soft mil-up">{AppData.footer.copy}</p>
                         </div>
 
+                    </div>
+                    <div className="col-md-7 col-lg-6 mil-mb-60 mt-5">
+                        <div className="row justify-content-end ">
+                            <div className="col-md-6 col-lg-7">
+                                <Link href="#" className="mil-button mil-arrow-place mil-btn-space ">
+                                    <span>Contact Us</span>
+                                    <ArrowIcon/>
+                                </Link>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </footer>
-    {/* footer end */}
+        {/* footer end */}
     </>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,14 +9,12 @@ import HeroOneSection from "@components/sections/HeroOne"
 import AboutSection from "@components/sections/About";
 import ServicesSection from "@components/sections/Services";
 import TeamSection from "@components/sections/Team";
-import LatestPostsSection from "@components/sections/LatestPosts";
 import Maps from "@components/sections/Maps";
 import Video from "@components/Video";
 import ProjectsMasonry from "@components/ProjectsMasonry";
 
 const TestimonialSlider = dynamic(() => import("@components/sliders/Testimonial"), {ssr: false});
 const PartnersSlider = dynamic(() => import("@components/sliders/Partners"), {ssr: false});
-const ProjectsSlider = dynamic(() => import("@components/sliders/Projects"), {ssr: false});
 
 const Home1 = (props) => {
     return (
@@ -25,12 +23,10 @@ const Home1 = (props) => {
             <Video videoId="46yXOSytDas"/>
             <AboutSection/>
             <ServicesSection/>
-            {/*<ProjectsSlider projects={props.projects}/>*/}
             <ProjectsMasonry projects={props.projects}/>
             <TestimonialSlider/>
             <PartnersSlider/>
             <TeamSection/>
-            {/*<LatestPostsSection posts={props.posts}/>*/}
             <Maps/>
         </Layouts>
     );
